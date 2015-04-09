773c3f7854d42b06493a0c3b0c39ca811b959bbc
Guillaume <gutrinh@gmail.com>
2015-04-09 12:11:49 +0200

    init security and user entity

--- src/Doge/FacebookBundle/Controller/DefaultController.php
+++ src/Doge/FacebookBundle/Controller/DefaultController.php
@@ -2,12 +2,22 @@
 
 namespace Doge\FacebookBundle\Controller;
 
+use Doge\FacebookBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        $user = new User();
+        $user->setAge(1);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist( $user );
+        $em->flush();
+
+        $em->remove( $user );
+
         return $this->render('DogeFacebookBundle:Default:index.html.twig', array('name' => $name));
     }
 }
