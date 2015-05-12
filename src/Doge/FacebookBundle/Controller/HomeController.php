b0930a1c6a118c2d2d1013302ae7a94cee5fcac1
Guillaume <gutrinh@gmail.com>
2015-05-12 18:10:34 +0200

    dump files in controller

--- src/Doge/FacebookBundle/Controller/HomeController.php
+++ src/Doge/FacebookBundle/Controller/HomeController.php
@@ -26,43 +26,45 @@ class HomeController extends Controller
         $form = $formBuilder->getForm();
         $message = "";
 
-        if( $request->getMethod() == "POST" && $this->getUser() ){
-            $form->handleRequest( $request );
+        print_r($_FILES);
 
-            if( !is_dir( $this->getImageDir() ) ){
-                mkdir( $this->getImageDir(), 0764, true );
-            }
-
-            $file = $form->get("file")->getData();
-
-//            $extension = $file->guessExtension();
-
-            $fileName = explode( ".", $file->getClientOriginalName() );
-            array_pop( $fileName );
-            $fileName = implode( $fileName, "." );
-
-            $form->get("file")->getData()->move( $this->getImageDir(), "1_" . $fileName . '.' . $extension );
-
-            try {
-                // Upload to a user's profile. The photo will be in the
-                // first album in the profile. You can also upload to
-                // a specific album by using /ALBUM_ID as the path
-                $response = (new FacebookRequest(
-                    $this->getUser()->getFacebookAccessToken(), 'POST', '/me/photos', array(
-                        'source' => new \CURLFile( $this->getImageDir() . DIRECTORY_SEPARATOR . $fileName, $file->getMimeType() ),
-                        'message' => 'A photo'
-                    )
-                ))->execute()->getGraphObject();
-
-                die;
-                // If you're not using PHP 5.5 or later, change the file reference to:
-                // 'source' => '@/path/to/file.name'
-                $message = "ok";
-
-            } catch(FacebookRequestException $e) {
-                $message = "error";
-            }
-        }
+//        if( $request->getMethod() == "POST" && $this->getUser() ){
+////            $form->handleRequest( $request );
+//
+//            if( !is_dir( $this->getImageDir() ) ){
+//                mkdir( $this->getImageDir(), 0764, true );
+//            }
+//
+//            $file = $form->get("file")->getData();
+//
+////            $extension = $file->guessExtension();
+//
+//            $fileName = explode( ".", $file->getClientOriginalName() );
+//            array_pop( $fileName );
+//            $fileName = implode( $fileName, "." );
+//
+//            $form->get("file")->getData()->move( $this->getImageDir(), "1_" . $fileName . '.' . $extension );
+//
+//            try {
+//                // Upload to a user's profile. The photo will be in the
+//                // first album in the profile. You can also upload to
+//                // a specific album by using /ALBUM_ID as the path
+//                $response = (new FacebookRequest(
+//                    $this->getUser()->getFacebookAccessToken(), 'POST', '/me/photos', array(
+//                        'source' => new \CURLFile( $this->getImageDir() . DIRECTORY_SEPARATOR . $fileName, $file->getMimeType() ),
+//                        'message' => 'A photo'
+//                    )
+//                ))->execute()->getGraphObject();
+//
+//                die;
+//                // If you're not using PHP 5.5 or later, change the file reference to:
+//                // 'source' => '@/path/to/file.name'
+//                $message = "ok";
+//
+//            } catch(FacebookRequestException $e) {
+//                $message = "error";
+//            }
+//        }
 
         return $this->render("DogeFacebookBundle:Home:upload.html.twig", [ 'form' => $form->createView(), "message" => $message ]);
     }
