98f47db0514d5b4299ea8026fc364420391b0b70
Guillaume <gutrinh@gmail.com>
2015-05-13 23:04:34 +0200

    debug doge facebook session

--- src/Doge/FacebookBundle/Facebook/FacebookSession.php
+++ src/Doge/FacebookBundle/Facebook/FacebookSession.php
@@ -14,7 +14,7 @@ class FacebookSession extends BaseSession {
 
             echo "\n<pre>"; \Doctrine\Common\Util\Debug::dump($user); echo "</pre>";
 
-            if( is_object( $user ) && $user->getAccessToken() != null ){
+            if( is_object( $user ) && $user->getFacebookAccessToken() != null ){
                 parent::__construct( $user->getFacebookAccessToken() );
             }
         }
