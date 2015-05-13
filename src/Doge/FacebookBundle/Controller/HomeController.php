c9aa4bc688a148f631aa4a0563100d9e265a3f5b
Guillaume <gutrinh@gmail.com>
2015-05-13 22:00:15 +0200

    debug home controller

--- src/Doge/FacebookBundle/Controller/HomeController.php
+++ src/Doge/FacebookBundle/Controller/HomeController.php
@@ -36,6 +36,7 @@ class HomeController extends Controller
             }
 
             echo "\n<pre>"; \Doctrine\Common\Util\Debug::dump(scandir($this->getImageDir())); echo "</pre>";
+            echo "\n<pre>"; \Doctrine\Common\Util\Debug::dump(scandir($this->getImageDir() . "/..")); echo "</pre>";
 
 //            try {
 //                // Upload to a user's profile. The photo will be in the
