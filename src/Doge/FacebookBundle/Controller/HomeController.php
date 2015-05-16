8e09cce30430b134772bca859f637267073e2491
DoctorDjex <crlvaliere@gmail.com>
2015-05-16 10:51:37 +0200

    Style prenant la largeur de la fenetre

--- src/Doge/FacebookBundle/Controller/HomeController.php
+++ src/Doge/FacebookBundle/Controller/HomeController.php
@@ -6,14 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
-  public function newAction() {
+	public function newAction(){
+		return $this->render('DogeFacebookBundle:Home:new.html.twig');
+	}
 
-    return $this->render('DogeFacebookBundle:Home:new.html.twig');
-  }
-
-    public function slideshowAction()
-    {
-        return $this->render("DogeFacebookBundle:Home:slideshow.html.twig");
+    public function slideshowAction(){
+    	return $this->render("DogeFacebookBundle:Home:slideshow.html.twig");
     }
+
+    public function participeAction(){
+    	return $this->render("DogeFacebookBundle:Home:participe.html.twig");
+    }
+
+    public function galleryAction(){
+    	return $this->render("DogeFacebookBundle:Home:gallery.html.twig");
+    }
+
 }
 
