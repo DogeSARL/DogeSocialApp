f64c0c169784164a5521eebfe71551402324ced5
sidney <shideneyu@gmail.com>
2015-06-16 18:00:24 +0200

    Fix: Birthdate was a date instead of a string

--- src/Doge/FacebookBundle/Entity/User.php
+++ src/Doge/FacebookBundle/Entity/User.php
@@ -44,7 +44,7 @@ class User extends BaseUser
     protected $prenom;
 
     /**
-     * @var \Datetime
+     * @var string
      * @ORM\Column(name="birthday", type="datetime", length=255, nullable=true)
      */
     protected $birthday;
