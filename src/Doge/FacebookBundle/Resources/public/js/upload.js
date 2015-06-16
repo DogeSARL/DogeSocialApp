afb3173c606dc0c7af3c77eee032febce50704a6
Guillaume <gutrinh@gmail.com>
2015-06-16 16:33:23 +0200

    debug

--- src/Doge/FacebookBundle/Resources/public/js/upload.js
+++ src/Doge/FacebookBundle/Resources/public/js/upload.js
@@ -4,25 +4,23 @@ var uploadApp = function(){
     function _init(){
         FB.api('/me/albums?fields=id,name', function(response) {
             var data = response.data;
-            console.log(reponse);
-            console.log(reponse.data);
-            //data.forEach(function( album ){
-            //    FB.api('/'+album.id+'/photos', function(photos){
-            //        if (photos && photos.data && photos.data.length){
-            //            var newAlbum = {};
-            //
-            //            for(var j=0; j<photos.data.length; j++){
-            //                var photo = photos.data[j];
-            //                // photo.picture contain the link to picture
-            //                newAlbum.push({ "name": photo.name, "picture":photo.picture });
-            //            }
-            //
-            //            newAlbum[ album.id ] = newAlbum;
-            //        }
-            //    });
-            //});
-            //
-            //console.log( albums );
+            data.forEach(function( album ){
+                FB.api('/'+album.id+'/photos', function(photos){
+                    if (photos && photos.data && photos.data.length){
+                        var newAlbum = {};
+
+                        for(var j=0; j<photos.data.length; j++){
+                            var photo = photos.data[j];
+                            // photo.picture contain the link to picture
+                            newAlbum.push({ "name": photo.name, "picture":photo.picture });
+                        }
+
+                        newAlbum[ album.id ] = newAlbum;
+                    }
+                });
+            });
+
+            console.log( albums );
         });
     }
 
