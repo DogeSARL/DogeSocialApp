d89b135be3883670af1a5ef2f29e85c24e1719c6
Guillaume <gutrinh@gmail.com>
2015-06-17 15:16:58 +0200

    integration gallery

--- src/Doge/FacebookBundle/Controller/GalleryController.php
+++ src/Doge/FacebookBundle/Controller/GalleryController.php
@@ -77,7 +77,8 @@ class GalleryController extends Controller{
 
                 $images[] = [ "url" => $response->getProperty("source"),
                     "name" => $response->getProperty("name"),
-                    "user" => $response->getProperty("from") ];
+                    "user" => $response->getProperty("from")
+                ];
 
             } catch( FacebookAuthorizationException $e ){
             }
