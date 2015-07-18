b1e17900f097f03325c47ef28035c2ea30537629
Guillaume <gutrinh@gmail.com>
2015-07-18 21:20:47 +0200

    debug image choice js

--- src/Doge/FacebookBundle/Controller/GalleryController.php
+++ src/Doge/FacebookBundle/Controller/GalleryController.php
@@ -31,6 +31,7 @@ class GalleryController extends Controller {
         $message = "";
 
         if ( $request->getMethod() == "POST" && $this->getUser() ) {
+            echo "\n<pre>"; var_dump($_POST); echo "</pre>";die;
             try {
                 $message = $this->get( "doge.form.handler.upload" )->handleRequest();
             } catch ( \Exception $e ) {
